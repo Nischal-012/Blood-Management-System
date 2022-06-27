26a106ec20d1fc3db5156353a4a288a887051693
Nischal Joshi <076bct051@ioepc.edu.np>
2022-06-27 18:53:31 +0545

    UserForms

---
+++ Blood Management System/UI/UsersForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blood_Management_System.UI
+{
+	public partial class UsersForm : Form
+	{
+		public UsersForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
