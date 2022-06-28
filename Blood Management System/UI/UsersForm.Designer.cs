a104e847f40315c40f8daa4434a8558451053034
Nischal Joshi <076bct051@ioepc.edu.np>
2022-06-28 15:37:36 +0545

    UsersForm

--- Blood Management System/UI/UsersForm.Designer.cs
+++ Blood Management System/UI/UsersForm.Designer.cs
@@ -28,12 +28,179 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UsersForm));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.developerLabel = new System.Windows.Forms.Label();
+			this.developedBy = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.topSidePanel = new System.Windows.Forms.Panel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.Fullname = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.topSidePanel.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.CornflowerBlue;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(1195, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(50, 47);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 13;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+			this.panel1.Controls.Add(this.developerLabel);
+			this.panel1.Controls.Add(this.developedBy);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(0, 645);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1246, 88);
+			this.panel1.TabIndex = 14;
+			// 
+			// developerLabel
+			// 
+			this.developerLabel.AutoSize = true;
+			this.developerLabel.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.developerLabel.Location = new System.Drawing.Point(684, 19);
+			this.developerLabel.Name = "developerLabel";
+			this.developerLabel.Size = new System.Drawing.Size(221, 45);
+			this.developerLabel.TabIndex = 3;
+			this.developerLabel.Text = "Nischal Joshi";
+			// 
+			// developedBy
+			// 
+			this.developedBy.AutoSize = true;
+			this.developedBy.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.developedBy.Location = new System.Drawing.Point(427, 19);
+			this.developedBy.Name = "developedBy";
+			this.developedBy.Size = new System.Drawing.Size(255, 45);
+			this.developedBy.TabIndex = 2;
+			this.developedBy.Text = "Developed By -";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.ForeColor = System.Drawing.Color.Firebrick;
+			this.label1.Location = new System.Drawing.Point(74, 19);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(335, 45);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "BloodBank Manager";
+			// 
+			// topSidePanel
+			// 
+			this.topSidePanel.BackColor = System.Drawing.Color.CornflowerBlue;
+			this.topSidePanel.Controls.Add(this.pictureBox2);
+			this.topSidePanel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.topSidePanel.Location = new System.Drawing.Point(0, 0);
+			this.topSidePanel.Name = "topSidePanel";
+			this.topSidePanel.Size = new System.Drawing.Size(1246, 86);
+			this.topSidePanel.TabIndex = 15;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(1197, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(48, 48);
+			this.pictureBox2.TabIndex = 0;
+			this.pictureBox2.TabStop = false;
+			// 
+			// Fullname
+			// 
+			this.Fullname.AutoSize = true;
+			this.Fullname.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.Fullname.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.Fullname.Location = new System.Drawing.Point(114, 103);
+			this.Fullname.Name = "Fullname";
+			this.Fullname.Size = new System.Drawing.Size(91, 25);
+			this.Fullname.TabIndex = 16;
+			this.Fullname.Text = "Full Name";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label2.Location = new System.Drawing.Point(114, 249);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(91, 25);
+			this.label2.TabIndex = 17;
+			this.label2.Text = "Full Name";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label3.Location = new System.Drawing.Point(114, 203);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(91, 25);
+			this.label3.TabIndex = 18;
+			this.label3.Text = "Full Name";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label4.Location = new System.Drawing.Point(114, 146);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(91, 25);
+			this.label4.TabIndex = 19;
+			this.label4.Text = "Full Name";
+			// 
+			// UsersForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1246, 733);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.Fullname);
+			this.Controls.Add(this.topSidePanel);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.pictureBox1);
+			this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "UsersForm";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.topSidePanel.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private PictureBox pictureBox1;
+		private Panel panel1;
+		private Label developerLabel;
+		private Label developedBy;
+		private Label label1;
+		private Panel topSidePanel;
+		private PictureBox pictureBox2;
+		private Label Fullname;
+		private Label label2;
+		private Label label3;
+		private Label label4;
 	}
 }
